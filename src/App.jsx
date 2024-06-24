8163c03a2f621a82adf80b65028a84bcf5a4f3ee
shuva9800 <ffshuva@gmail.com>
2024-06-24 11:06:36 +0530

    reactproject1to3

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+      <h1>Hello, Good Morning</h1>
+
+      <p>This is simole jsx</p>
+   </div>
+  )
+}
+
+export default App
